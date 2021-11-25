c22401c3260d896a63dda3505df6b6c23920f93b
Thuviksa <90987157+ThuviksaM@users.noreply.github.com>
2021-11-25 12:02:50 -0500

    Update and rename index.js to site.js

--- docs/site.js
+++ docs/site.js
@@ -12,9 +12,9 @@ var i = 0;
 var txt = 'Welcome!'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+function typeWriter(id) {
   if (i < txt.length) {
-    document.getElementById("indexHR").innerHTML += txt.charAt(i);
+    document.getElementById(id).innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
