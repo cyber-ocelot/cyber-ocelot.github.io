09a9ba40492a47678f7ccd14831e348fc8c3dd46
Thuviksa <90987157+ThuviksaM@users.noreply.github.com>
2021-11-25 11:59:31 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,21 @@
+/*--------------------------------------------------------------------------------------*/
 
-
+/* Animated menu icon */
 function aniMenu(x) {
   x.classList.toggle("change");
+}
+
+/*--------------------------------------------------------------------------------------*/
+
+/* Typewrtier effect */
+var i = 0;
+var txt = 'Welcome!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("indexHR").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
